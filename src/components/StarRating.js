514676e7c6d72018636dcea478f0f5d459ad81de
BillStephens2022 <two4onebill@yahoo.com>
2024-07-12 16:34:18 -0400

    added additional props to StarRating component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 import Star from "./Star";
 
 const containerStyle = {
@@ -11,21 +12,42 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetRating: PropTypes.func,
 };
 
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [hoverRating, setHoverRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -34,10 +56,12 @@ export default function StarRating({ maxRating = 5 }) {
             isFull={hoverRating ? hoverRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setHoverRating(i + 1)}
             onHoverOut={() => setHoverRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{hoverRating || rating || ""}</p>
+      <p style={textStyle}>{messages.length === maxRating ? messages[hoverRating ? hoverRating - 1 : rating - 1] : hoverRating || rating || ""}</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import StarRating from './components/StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}/>
     <StarRating maxRating={10} />
-    <StarRating /> */}
+    <StarRating size={24} color="red" className="test" defaultRating={3} /> */}
     <App />
   </React.StrictMode>
 );
