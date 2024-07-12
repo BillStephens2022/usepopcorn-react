30c38f2a66ebed8247c52586018bacfea75e9d51
BillStephens2022 <two4onebill@yahoo.com>
2024-07-12 07:40:46 -0400

    refactored ListBox component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import { tempMovieData } from "./data/movieData";
 import Logo from "./components/Logo";
 import NumResults from "./components/NumResults";
 import SearchBar from "./components/SearchBar";
-
+import ListBox from "./components/ListBox";
+import WatchedBox from "./components/WatchedBox";
+import MovieList from "./components/MovieList";
+import { tempMovieData } from "./data/movieData";
 
 export default function App() {
-
   const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
       <Navbar>
@@ -17,8 +19,13 @@ export default function App() {
         <SearchBar />
         <NumResults movies={movies} />
       </Navbar>
-      <Main movies={movies} />
-      
+
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
