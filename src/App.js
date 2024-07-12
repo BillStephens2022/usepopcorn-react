e77df5190f0c803a4baa9fd352e772d30f9e5532
BillStephens2022 <two4onebill@yahoo.com>
2024-07-12 07:27:54 -0400

    refactored Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-
+import { tempMovieData } from "./data/movieData";
+import Logo from "./components/Logo";
+import NumResults from "./components/NumResults";
+import SearchBar from "./components/SearchBar";
 
 
 export default function App() {
 
-
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <Navbar />
-      <Main />
+      <Navbar>
+        <Logo />
+        <SearchBar />
+        <NumResults movies={movies} />
+      </Navbar>
+      <Main movies={movies} />
       
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,7 @@
-import Logo from "./Logo";
-import NumResults from "./NumResults";
-import SearchBar from "./SearchBar";
-
-export default function Navbar() {
+export default function Navbar({ children }) {
   return (
     <nav className="nav-bar">
-      <Logo />
-      <SearchBar />
-      <NumResults />
+      {children}
     </nav>
   );
 }
