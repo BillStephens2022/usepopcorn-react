698841be70b979c6843c3d18d7af2a3335d91def
BillStephens2022 <two4onebill@yahoo.com>
2024-07-13 06:53:48 -0400

    added a Loader component to render when data is loading

--- src/App.js
+++ src/App.js
@@ -8,17 +8,28 @@ import Box from "./components/Box";
 import MovieList from "./components/MovieList";
 import WatchedMovieList from "./components/WatchedMovieList";
 import WatchedSummary from "./components/WatchedSummary";
+import Loader from "./components/Loader";
 import { tempMovieData } from "./data/movieData";
 import { tempWatchedData } from "./data/movieData";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const query = "pulp fiction";
+
+  async function fetchMovies() {
+    setIsLoading(true);
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${query}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    setIsLoading(false);
+  }
 
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=pulp fiction`).then((response) => response.json()).then((data) => setMovies(data.Search));
+    fetchMovies();
   }, []);
-     
 
   return (
     <>
@@ -30,7 +41,7 @@ export default function App() {
 
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {isLoading ? <Loader /> : <MovieList movies={movies} />}
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
