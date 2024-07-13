1db27c57fc032daacee17ccd65d5a35e406912e3
BillStephens2022 <two4onebill@yahoo.com>
2024-07-13 08:04:42 -0400

    moved function for fetching movies inside the useEffect to eliminate warning

--- src/App.js
+++ src/App.js
@@ -13,41 +13,42 @@ import ErrorMessage from "./components/ErrorMessage";
 // import { tempMovieData } from "./data/movieData";
 // import { tempWatchedData } from "./data/movieData";
 
+const tempQuery = "Pulp Fiction";
+
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(tempQuery);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
-  const tempQuery = "pulp fiction";
-
-  async function fetchMovies() {
-    setIsLoading(true);
-    setError("");
-    try {
-      const response = await fetch(
-        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${query}`
-      );
-
-      if (!response.ok) {
-        throw new Error("Something went wrong with fetching movies!");
-      }
-      
-      const data = await response.json();
-
-      if (data.Response === "False") throw new Error("Movie not found!");
-
-      setMovies(data.Search);
-    } catch (error) {
-      console.error(error.message);
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }
+  
 
   useEffect(() => {
+    async function fetchMovies() {
+      setIsLoading(true);
+      setError("");
+      try {
+        const response = await fetch(
+          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${query}`
+        );
+  
+        if (!response.ok) {
+          throw new Error("Something went wrong with fetching movies!");
+        }
+        
+        const data = await response.json();
+  
+        if (data.Response === "False") throw new Error("Movie not found!");
+  
+        setMovies(data.Search);
+      } catch (error) {
+        console.error(error.message);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
     if (query.length < 3) {
       setMovies([]);
       setError("");
